52e56fbee4e3c53599ba4f338a57da05138618e7
nicolascalegari <nacalegari@hotmail.com>
2026-09-11 15:53:24 -0300

    Atualizando arquivos

---
+++ Cap_06/exemplo_6.21.py
@@ -0,0 +1,13 @@
+tabela = {"Alface": 0.45,   
+          "Batata": 1.20,
+          "Tomate": 2.30,
+          "Feijao": 1.50}
+
+while True:
+    produto = input("Digite o nome do produto ou 'fim' para terminar: ")
+    if produto == "fim":
+        break
+    if produto in tabela:
+        print(f"Preço {tabela[produto]:5.2f}")
+    else:
+        print(f"Produto não encontrado")
